41ec58e654d7b6f644aa845a2fd71383a11e77de
wadad zein <wadad_zein@hotmail.com>
2021-04-15 18:45:33 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ tasklist.innerHTML+=`<li>${newTask}
 <button data-set="delete">X for delete</button>
 </li>
 tasklist.addEventListener("click", function(e){
-  if e.traget.dataset=true{
+  if e.traget.dataset==="delete;{
     e.traget.parentEleemt.remove();
   }
 }
