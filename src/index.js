afd670773e9288900abdc868c3b22de3da09c03a
wadad zein <wadad_zein@hotmail.com>
2021-04-15 18:42:13 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -8,7 +8,10 @@ const tasklist= document.querySelector("#tasks");
 taskForm.addEventListener("submit", function (e){
 e.preventDefault();
 const newTask= document.querySelector("#new-task-description").value;
-tasklist.innerHTML+=`<li>${newTask}<button data-set="delete">X for delete<button></li>`;
+tasklist.innerHTML+=`<li>${newTask}
+<button data-set="delete">X for delete<button>
+
+</li>`;
 
 
 
