328dfdedb9be118145db8fa4bbf3e682dbe3d416
wadad zein <wadad_zein@hotmail.com>
2021-04-15 19:17:21 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+const form= document.getElementById("create-task-form");
+const list= document.getElementById("list");
+
+form.addEventListener("submit",function(e){
+const todos= document.getElementById("new-task-description").value;
+e.preventDefault();
+list.innerHTML+= `<li>
+${todos}
+<button data-set="delete">X for delete</button>
+</li>`;
+
+form.reset();
+});
+
+list.addEventListener("click", function(e){
+  if(e.target.dataset.set==="delete"){
+    e.target.parentElement.remove();
+  }
+  
+});
+
 });
