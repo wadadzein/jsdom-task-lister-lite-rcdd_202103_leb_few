c6e15f79cda813571302dc7a4851c9ff68735901
wadad zein <wadad_zein@hotmail.com>
2021-04-15 18:47:21 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -11,13 +11,14 @@ const newTask= document.querySelector("#new-task-description").value;
 tasklist.innerHTML+=`<li>${newTask}
 <button data-set="delete">X for delete</button>
 </li>
+`;
 tasklist.addEventListener("click", function(e){
-  if (e.traget.dataset.set==="delete) {
+  if (e.traget.dataset.set==="delete") {
     e.traget.parentEleemt.remove();
   }
 }
+);
 
-`;
 
 
 
